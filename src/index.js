9093d93a86160e931eb589c4b3ef1b5b886d681e
jomariepolanco <jomariepolanco@gmail.com>
2020-11-17 18:46:47 -0500

    done except for extra deliverables

--- src/index.js
+++ src/index.js
@@ -1,3 +1,38 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  const taskForm = document.querySelector('#create-task-form')
+
+  function addTask() {
+    taskForm.addEventListener('submit', function(e) {
+      e.preventDefault()
+      // console.log(e)
+      const input = e.target['new-task-description'].value
+      // console.log(input)
+      // debugger
+      const newTask = document.querySelector('#tasks')
+      const li = document.createElement("li")
+      li.textContent = input 
+      newTask.append(li)
+
+      const deleteButton = document.createElement("button")
+      deleteButton.className = 'delete-button'
+      deleteButton.dataset.action = "delete"
+      deleteButton.textContent = "X"
+      li.append(deleteButton)
+
+      deleteButton.addEventListener("click", function() {
+        li.remove()
+      })
+    })
+    
+
+  }
+
+  addTask()
+
+  const priority = document.createElement('collection-select')
+
+
+
+
+}); // end of dOMContentLoaded
